forerunnergames/coa

commit 8df75f3b045e837eeeadf23bdb6122c80891acd0

Improve logging & debug text printing. (#61)
- Export all top-level log levels to the Godot editor for easy debugging
  from the editor UI.

- Inject all top-level log levels recursively down into every dependent
  component having its own log, so that log levels are homogeneous.

- Allow printing of static messages for in-game debugging text in
  Player.cs, that is enabled / disabled via the T key on the keyboard.
  This allows for a message that is not being updated every frame to not
  disappear after a single frame and actually persist on the screen
  until some future state decides to change it. Static messages can also
  be removed. (Printing of dynamic / per-frame-updated messages are
  already supported & in use).

scripts/Weapon.cs
@@ -31,13 +31,13 @@ public class Weapon
 
   // @formatter:on
 
-  public Weapon (Node player)
+  public Weapon (Node player, Log.Level logLevel)
   {
     _playerAnimator1 = player.GetNode <AnimationPlayer> ("Sprites/AnimationPlayer1");
     _playerAnimator2 = player.GetNode <AnimationPlayer> ("Sprites/AnimationPlayer2");
     _backpackSprite = player.GetNode <Sprite> ("Sprites/backpack");
     _itemInBackpackSprite = player.GetNode <Sprite> ("Sprites/item-in-backpack");
-    InitializeStateMachine();
+    InitializeStateMachine (logLevel);
   }
 
   // ReSharper disable once MemberCanBeMadeStatic.Global
@@ -118,9 +118,9 @@ public class Weapon
     }
   }
 
-  private void InitializeStateMachine()
+  private void InitializeStateMachine (Log.Level logLevel)
   {
-    _sm = new StateMachine <State> (TransitionTable, State.Unequipped);
+    _sm = new StateMachine <State> (TransitionTable, State.Unequipped) { LogLevel = logLevel };
     _sm.OnTransition (State.Unequipped, State.Equipped, StartEquipping);
     _sm.OnTransition (State.Equipped, State.Unequipped, StartUnequipping);
     _sm.AddTrigger (State.Unequipped, State.Equipped, ShouldEquip);

scenes/main.tscn
@@ -4799,7 +4799,7 @@ __meta__ = {
 "_edit_lock_": true
 }
 FlightPathColor = Color( 1, 0, 0, 1 )
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 1"]
 visible = false
@@ -4839,7 +4839,7 @@ __meta__ = {
 "_edit_lock_": true
 }
 FlightPathColor = Color( 0, 1, 0, 1 )
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 2"]
 visible = false
@@ -4879,7 +4879,7 @@ __meta__ = {
 "_edit_lock_": true
 }
 FlightPathColor = Color( 0, 0, 1, 1 )
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 3"]
 visible = false
@@ -4919,7 +4919,7 @@ __meta__ = {
 "_edit_lock_": true
 }
 FlightPathColor = Color( 1, 0.701961, 0, 1 )
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 4"]
 visible = false
@@ -4959,7 +4959,7 @@ __meta__ = {
 "_edit_lock_": true
 }
 FlightPathColor = Color( 1, 0.376471, 0, 1 )
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 5"]
 visible = false
@@ -5000,7 +5000,7 @@ __meta__ = {
 }
 FlightPathColor = Color( 0, 0.952941, 1, 1 )
 NearestPerchFrequency = 1.0
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 6"]
 visible = false
@@ -5041,7 +5041,7 @@ __meta__ = {
 }
 FlightPathColor = Color( 0.0117647, 0.196078, 0.0862745, 1 )
 NearestPerchFrequency = 1.0
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 7"]
 visible = false
@@ -5082,7 +5082,7 @@ __meta__ = {
 }
 FlightPathColor = Color( 0, 0.670588, 1, 1 )
 NearestPerchFrequency = 1.0
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 8"]
 visible = false
@@ -5123,7 +5123,7 @@ __meta__ = {
 }
 FlightPathColor = Color( 0.415686, 0.32549, 0.117647, 1 )
 NearestPerchFrequency = 1.0
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 9"]
 visible = false
@@ -5164,7 +5164,7 @@ __meta__ = {
 }
 FlightPathColor = Color( 1, 0.588235, 0.384314, 1 )
 NearestPerchFrequency = 1.0
-LogLevel = 2
+LogLevel = 0
 
 [node name="PerchingCollider" type="Area2D" parent="Butterfly 10"]
 visible = false

scripts/Butterfly.cs
@@ -171,7 +171,7 @@ public class Butterfly : AnimatedSprite
     _drawRect = delegate (Rect2 rect, Color color, bool filled) { DrawRect (rect, color, filled); };
     _idleTimer = GetNode <Timer> ("IdleTimer");
     _lastPosition = Position;
-    _stateMachine = new StateMachine <State> (TransitionTable, State.Flying, Name);
+    _stateMachine = new StateMachine <State> (TransitionTable, State.Flying, Name) { LogLevel = LogLevel };
     _stateMachine.OnTransitionTo (State.Perching, () => Animation = PerchingAnimation);
     _stateMachine.OnTransitionTo (State.Evading, () => Animation = EvadingAnimation);
     _stateMachine.OnTransitionTo (State.Flying, () => Animation = FlyingAnimation);

scripts/Cliffs.cs
@@ -8,6 +8,7 @@ public class Cliffs : Area2D
 {
   [Export] public Season InitialSeason = Season.Summer;
   [Export] public Color InitialClearColor = Color.Color8 (11, 118, 255);
+  [Export] public Log.Level LogLevel = Log.Level.Info;
 
   // Field must be publicly accessible from Player.cs
   public Season CurrentSeason;
@@ -50,7 +51,7 @@ public class Cliffs : Area2D
   public override void _Ready()
   {
     // ReSharper disable once ExplicitCallerInfoArgument
-    _log = new Log (Name);
+    _log = new Log (Name) { CurrentLevel = LogLevel };
     _clearColor = InitialClearColor;
     VisualServer.SetDefaultClearColor (_clearColor);
     _waterfallZIndex.Add (Season.Summer, 33);

scripts/Log.cs
@@ -17,7 +17,7 @@ public class Log
     All
   }
 
-  public Log ([CallerFilePath] string name = "") { _name = Path.GetFileNameWithoutExtension (name); }
+  public Log ([CallerFilePath] string name = "") => _name = Path.GetFileNameWithoutExtension (name);
   public bool All (string message) => CurrentLevel > Level.Debug && Print (message, Level.All);
   public bool Debug (string message) => CurrentLevel > Level.Info && Print (message, Level.Debug);
   public bool Info (string message) => CurrentLevel > Level.Warn && Print (message, Level.Info);

scripts/Player.cs
@@ -57,6 +57,7 @@ public class Player : KinematicBody2D
   [Export] public int EnergyMeterReplenishTimeSeconds = 10;
   [Export] public int EnergyMeterDepletionTimeSeconds = 3;
   [Export] public State InitialState = State.Idle;
+  [Export] public Log.Level LogLevel = Log.Level.Info;
 
   // Field must be publicly accessible from Cliffs.cs
   public bool IsInCliffs;
@@ -98,7 +99,8 @@ public class Player : KinematicBody2D
   private float _energyMeterDepletionRatePerUnit;
   private bool _isFlippedHorizontally;
   private bool _wasFlippedHorizontally;
-  private readonly List <string> _printLines = new();
+  private readonly List <string> _printLinesOnce = new();
+  private readonly List <string> _printLinesContinuously = new();
   private IStateMachine <State> _stateMachine = null!;
   private ulong _fallingStartTimeMs;
   private ulong _elapsedFallingTimeMs;
@@ -543,9 +545,9 @@ public class Player : KinematicBody2D
   public override void _Ready()
   {
     // ReSharper disable once ExplicitCallerInfoArgument
-    _log = new Log (Name);
+    _log = new Log (Name) { CurrentLevel = LogLevel };
     _rng.Randomize();
-    _weapon = new Weapon (this);
+    _weapon = new Weapon (GetNode <Node2D> ("Sprites"), LogLevel);
     _camera = GetNode <Camera2D> ("Camera2D");
     _rayChest = GetNode <RayCast2D> ("Chest");
     _rayFeet = GetNode <RayCast2D> ("Feet");
@@ -628,7 +630,7 @@ public class Player : KinematicBody2D
     if (Mathf.Abs (_velocity.y) < VelocityEpsilon) _velocity.y = 0.0f;
     _velocity = MoveAndSlide (_velocity, Vector2.Up);
     CalculateFallingStats();
-    PrintLine (DumpState());
+    PrintLineOnce (DumpState());
     Print();
 
     // @formatter:off
@@ -1042,7 +1044,9 @@ public class Player : KinematicBody2D
   private bool IsMovingUp() => _velocity.y + VelocityEpsilon < 0.0f;
   private bool IsMovingDown() => _velocity.y - VelocityEpsilon > 0.0f;
   private float GetFallingTimeSeconds() => _elapsedFallingTimeMs > 0 ? _elapsedFallingTimeMs / 1000.0f : _lastTotalFallingTimeMs / 1000.0f;
-  private void PrintLine (string line) => _printLines.Add (line);
+  private void PrintLineOnce (string line) => _printLinesOnce.Add (line);
+  private void PrintLineContinuously (string line) => _printLinesContinuously.Add (line);
+  private void StopPrintingContinuousLine (string line) => _printLinesContinuously.Remove (line);
   // @formatter:on
 
   private void HorizontalVelocity()
@@ -1216,8 +1220,9 @@ public class Player : KinematicBody2D
     _label.Text = "";
     _label.BbcodeText = "";
     _label.GetVScroll().Visible = false;
-    foreach (var line in _printLines) _label.AddText (line + "\n");
-    _printLines.Clear();
+    foreach (var line in _printLinesContinuously) _label.AddText (line + "\n");
+    foreach (var line in _printLinesOnce) _label.AddText (line + "\n");
+    _printLinesOnce.Clear();
   }
 
   // @formatter:off
@@ -1284,7 +1289,7 @@ public class Player : KinematicBody2D
   private void InitializeStateMachine()
   {
     // @formatter:off
-    _stateMachine = new StateMachine <State> (TransitionTable, InitialState);
+    _stateMachine = new StateMachine <State> (TransitionTable, InitialState) { LogLevel = LogLevel };
     _stateMachine.OnTransitionFrom (State.ReadingSign, StopReadingSign);
     _stateMachine.OnTransitionTo (State.ReadingSign, ReadSign);
     _stateMachine.OnTransitionTo (State.Traversing, () => _animationPlayer.Play (TraversingAnimation));
@@ -1353,7 +1358,8 @@ public class Player : KinematicBody2D
       if (!_audio.Playing) return;
 
       _audio.Stop();
-      PrintLine ("Sound effects: Stopped cliff arresting sound.");
+      StopPrintingContinuousLine ("Sound effects: Playing cliff arresting sound.");
+      PrintLineContinuously ("Sound effects: Stopped cliff arresting sound.");
     });
 
     _stateMachine.OnTransitionTo (State.CliffArresting, () =>
@@ -1364,7 +1370,8 @@ public class Player : KinematicBody2D
       if (_audio.Playing) return;
 
       _audio.Play();
-      PrintLine ("Sound effects: Playing cliff arresting sound.");
+      StopPrintingContinuousLine ("Sound effects: Stopped cliff arresting sound.");
+      PrintLineContinuously ("Sound effects: Playing cliff arresting sound.");
     });
 
     _stateMachine.OnTransition (State.ClimbingPrep, State.Idle, () =>

scripts/StateMachine.cs
@@ -26,6 +26,7 @@ public class StateMachine <T> : IStateMachine <T> where T : struct, Enum
   private readonly List <T> _triggeredToStates = new();
   private readonly List <T> _fromStates = new();
   private readonly List <T> _toStates = new();
+  private bool _isFirstUpdate = true;
   private readonly Log _log;
 
   // ReSharper disable once ExplicitCallerInfoArgument
@@ -71,7 +72,6 @@ public class StateMachine <T> : IStateMachine <T> where T : struct, Enum
     _currentState = initialState;
     _parentState = initialState;
     _transitionTable = transitionTable;
-    _log.Info ($"Initial state: {ToString (_initialState)}");
   }
 
   public T GetState() => _currentState;
@@ -123,6 +123,12 @@ public class StateMachine <T> : IStateMachine <T> where T : struct, Enum
 
   public void Update()
   {
+    if (_isFirstUpdate)
+    {
+      _log.Info ($"Initial state: {ToString (_initialState)}");
+      _isFirstUpdate = false;
+    }
+
     if (!_triggers.ContainsKey (_currentState) && !_triggers.ContainsKey (AnyState)) return;
 
     _fromStates.Clear();

commit d9814b159ec6a5dbf0769a45350e2a31e96a0176

Fix player weapon equipping bug.
Problem:

  - Pressing the E key on the keyboard to equip the player's weapon
    crashes the game. This is a regression caused by #61 that
    inadvertently passed the "Sprites" node into the Weapon.cs
    constructor instead of the "Player" node that was expected.

  - Modify the Weapon.cs constructor to use the "Sprites" node instead
    of the entire "Player" node, which simplifies the code and utilizes
    the Principle of Least Privilege.

scripts/Weapon.cs
@@ -31,12 +31,12 @@ public class Weapon
 
   // @formatter:on
 
-  public Weapon (Node player, Log.Level logLevel)
+  public Weapon (Node sprites, Log.Level logLevel)
   {
-    _playerAnimator1 = player.GetNode <AnimationPlayer> ("Sprites/AnimationPlayer1");
-    _playerAnimator2 = player.GetNode <AnimationPlayer> ("Sprites/AnimationPlayer2");
-    _backpackSprite = player.GetNode <Sprite> ("Sprites/backpack");
-    _itemInBackpackSprite = player.GetNode <Sprite> ("Sprites/item-in-backpack");
+    _playerAnimator1 = sprites.GetNode <AnimationPlayer> ("AnimationPlayer1");
+    _playerAnimator2 = sprites.GetNode <AnimationPlayer> ("AnimationPlayer2");
+    _backpackSprite = sprites.GetNode <Sprite> ("backpack");
+    _itemInBackpackSprite = sprites.GetNode <Sprite> ("item-in-backpack");
     InitializeStateMachine (logLevel);
   }
 