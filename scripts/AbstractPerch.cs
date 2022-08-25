88574c53643b2d61c679d6a0ff30f3bea6f63914
Aaron Mahan <knightofiam@gmail.com>
2022-08-25 17:06:38 -0600

    Various minor cleanups.
    - Convert unused string interpolations to literals.
    - Remove unused imports.
    - Organize imports.

--- scripts/AbstractPerch.cs
+++ scripts/AbstractPerch.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using static Tools;
-using GetLocalTransform = Tools.GetLocalTransform;
 
 public abstract class AbstractPerch : IPerchable
 {
